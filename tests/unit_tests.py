8d74561125350acccae6ba09bc51f326d973c262
Primaux78 <brice.romanet@gmail.com>
2026-04-03 11:42:14 +0200

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -8,18 +8,18 @@ class PyCalcUnitTests(unittest.TestCase):
     while i < b:
       res += a
     return a
-    
-  def add(a, b):
-    res = a
-    if b > 0:
-        while (b - 1) >= 0:
-            b -= 1
-            res += 1
-    elif b < 0:
-        while (b + 1) <= 0:
-            b += 1
-            res -= 1
-    return res
+
+  def test_multi(self):
+    left = [5]
+    right = [9]
+    expected = [45]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.add(l, r)
+        self.assertEqual(got, e)
 
   
   def test_add(self):
